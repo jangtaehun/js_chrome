eb77a59ccb65bf941b73b2c9255d1632d44fbdc7
Jangtaehun <jangth0056@naver.com>
2024-02-08 09:51:15 +0900

    team03장태훈

--- login.js
+++ login.js
@@ -1,6 +1,9 @@
 const loginFrom = document.querySelector("#login-form");
 const loginInput = loginFrom.querySelector("input");
 const loginSubmit = loginFrom.querySelector("input[type=submit]");
+const greeting = document.getElementById("greeting");
+
+const HIDDEM_CLASSNAME = "hidden";
 
 function loginBtnClick() {
     const namePattern = /^[가-힣a-zA-Z]+$/;
@@ -8,8 +11,6 @@ function loginBtnClick() {
 
     if (!namePattern.test(username)) {
         alert("Please write your korean or english name");
-    } else {
-        alert(`Hi ${username}!!`);
     }
 }
 loginSubmit.addEventListener("click", loginBtnClick);
@@ -19,7 +20,10 @@ function onLoginSybmit(event) {
     // 브라우저가 기본 동작을 실행하지 못하게 막기 / event object는 preventDefault 함수를 기본적으로 갖고 있음 / 첫 arument는 지금 막 벌어진 event들에 대한 정보를 갖고 있다
     // 어떤 event의 기본 행동이든지 발생되지 않도록 막는다.
     // event는 EventListener 함수의 첫 번째 argument 안에 있는 function / EventListener에 어떤 function을 추가하든 js는 첫 번째 argument로 발생된 event에 대한 정보를 준다.
-    console.log(event);
+    const username = loginInput.value;
+    loginFrom.classList.add(HIDDEM_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEM_CLASSNAME);
 }
 loginFrom.addEventListener("submit", onLoginSybmit);
 // addEventListener에 있는 함수는 브라우저가 실행시키고 event에 대한 정보도 담아준다.
